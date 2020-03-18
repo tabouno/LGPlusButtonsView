a14a0d38d86fb9f7576f1dbfdd3282104c428af9
itaboada <itaboada@nts-solutions.com>
2020-03-18 13:14:46 +0100

    ignore hidden button in hitTest (gaimanjing)

--- LGPlusButtonsView/LGPlusButtonsView.m
+++ LGPlusButtonsView/LGPlusButtonsView.m
@@ -315,6 +315,10 @@ typedef NS_ENUM(NSUInteger, LGPlusButtonDescriptionsPosition)
 
     for (LGPlusButton *button in _buttonsArray)
     {
+    	if (!button.isShowing) {
+            continue;
+        }
+    	
         CGPoint newPoint = [self convertPoint:point toView:button];
 
         view = [button hitTest:newPoint withEvent:event];
